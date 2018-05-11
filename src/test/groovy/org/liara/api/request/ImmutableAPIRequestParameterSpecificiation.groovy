151a3628defc733383fd19e39b2e3c64998d88df
Cédric DEMONGIVERT <cedric.demongivert@gmail.com>
2018-05-11 12:23:56 -0400

    test support

---
+++ src/test/groovy/org/liara/api/request/ImmutableAPIRequestParameterSpecificiation.groovy
@@ -0,0 +1,15 @@
+package org.liara.api.request
+
+import spock.lang.Specification
+
+class ImmutableAPIRequestParameterSpecificiation extends Specification
+{
+  def "plopl" () {
+    given: "a consumer"
+     
+    and: "a consumer mixin"
+      
+    expect: "the mixin's publisher set must be empty"
+      null == null
+  }
+}
